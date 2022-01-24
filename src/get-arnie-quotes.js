45187f0725a9942e73d4c9d4536e646ee4ae3764
ShaunB <shaun.bettridge@gmail.com>
2022-01-24 17:26:59 -0500

    Implementation of getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,28 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // Get quote from each url in array
+  // Return array of responses
+  return Promise.allSettled(urls.map( url => httpGet(url)))
+    .then(processQuoteResponses) 
 };
+
+
+const processQuoteResponses = responses => {
+  // Process the responses into the required format
+  const quotesArray = [];
+  
+  for (const response of responses){
+    const responseBody = JSON.parse(response.value.body);
+    
+    if (response.value.status == 200){
+      quotesArray.push({"Arnie Quote": responseBody.message});
+    } else{ 
+      quotesArray.push({"FAILURE": responseBody.message});
+    }  
+  }
+  return quotesArray; 
+}
 
 module.exports = {
   getArnieQuotes,
